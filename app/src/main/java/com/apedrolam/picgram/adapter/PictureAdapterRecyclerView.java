46c31e319dd7f1ea8f18255270bb2ecd651f3b72
Albert Pedrola <abert.pedrola@netcentric.biz>
2017-08-16 22:53:40 +0200

    Cardview

---
+++ app/src/main/java/com/apedrolam/picgram/adapter/PictureAdapterRecyclerView.java
@@ -0,0 +1,69 @@
+package com.apedrolam.picgram.adapter;
+
+import android.app.Activity;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.apedrolam.picgram.R;
+import com.apedrolam.picgram.model.Picture;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+
+/**
+ * Created by albertpedrola on 16/08/2017.
+ */
+
+public class PictureAdapterRecyclerView extends RecyclerView.Adapter<PictureAdapterRecyclerView.PictureViewHolder> {
+
+    private ArrayList<Picture> pictures;
+    private int resource;
+    private Activity activiity;
+
+    public PictureAdapterRecyclerView(ArrayList<Picture> pictures, int resource, Activity activiity) {
+        this.pictures = pictures;
+        this.resource = resource;
+        this.activiity = activiity;
+    }
+
+    @Override
+    public PictureViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(resource, parent, false);
+        return new PictureViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(PictureViewHolder holder, int position) {
+        Picture picture = pictures.get(position);
+        holder.usernameCard.setText(picture.getUserName());
+        holder.timeCard.setText(picture.getTime());
+        holder.likeNumberCard.setText(picture.getLike_number());
+    }
+
+    @Override
+    public int getItemCount() {
+        return pictures.size();
+    }
+
+    public class PictureViewHolder extends RecyclerView.ViewHolder {
+
+        private ImageView pictureCard;
+        private TextView usernameCard;
+        private TextView timeCard;
+        private TextView likeNumberCard;
+
+        public PictureViewHolder(View itemView) {
+            super(itemView);
+
+            pictureCard = (ImageView) itemView.findViewById(R.id.pictureCard);
+            usernameCard = (TextView) itemView.findViewById(R.id.userNameCard);
+            timeCard = (TextView) itemView.findViewById(R.id.timeCard);
+            likeNumberCard = (TextView) itemView.findViewById(R.id.likeNumberCard);
+        }
+    }
+}
--- app/src/main/java/com/apedrolam/picgram/view/fragments/HomeFragment.java
+++ app/src/main/java/com/apedrolam/picgram/view/fragments/HomeFragment.java
@@ -3,11 +3,19 @@ package com.apedrolam.picgram.view.fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.apedrolam.picgram.R;
+import com.apedrolam.picgram.adapter.PictureAdapterRecyclerView;
+import com.apedrolam.picgram.model.Picture;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -24,7 +32,35 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        showToolbar(getResources().getString(R.string.toolbar_title_home), false, view);
+        RecyclerView picturesRecycler = (RecyclerView) view.findViewById(R.id.pictureRecycler);
+
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext());
+        linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+
+        picturesRecycler.setLayoutManager(linearLayoutManager);
+
+        PictureAdapterRecyclerView pictureAdapterRecyclerView = new PictureAdapterRecyclerView(buildPictures(), R.layout.cardview_picture, getActivity());
+
+        picturesRecycler.setAdapter(pictureAdapterRecyclerView);
+
+        return view;
+    }
+
+    public ArrayList<Picture> buildPictures() {
+        ArrayList<Picture> pictures = new ArrayList<>();
+        pictures.add(new Picture("http://www.novalandtours.com/images/guide/guilin.jpg", "Albert Pedrola", "4 days", "3"));
+        pictures.add(new Picture("http://www.enjoyart.com/library/landscapes/tuscanlandscapes/large/Tuscan-Beidge--by-Art-Fronckowiak-.jpg", "Maria Rojas", "6 days", "25"));
+        pictures.add(new Picture("http://www.educationquizzes.com/library/KS3-Geography/river-1-1.jpg", "Albert Pedrola", "8 days", "14"));
+        return pictures;
+    }
+
+    public void showToolbar(String title, boolean upButton, View view) {
+        Toolbar toolbar = (Toolbar) view.findViewById(R.id.toolbar);
+        ((AppCompatActivity) getActivity()).setSupportActionBar(toolbar);
+        ((AppCompatActivity) getActivity()).getSupportActionBar().setTitle(title);
+        ((AppCompatActivity) getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(upButton);
     }
 
 }
