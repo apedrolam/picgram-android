a002dd1645829b7d37b94737f58d242b56f9e653
Albert Pedrola <abert.pedrola@netcentric.biz>
2017-08-16 11:31:15 +0200

    Create account activity created

---
+++ app/src/main/java/com/apedrolam/picgram/view/CreateAccountActivity.java
@@ -0,0 +1,15 @@
+package com.apedrolam.picgram.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.apedrolam.picgram.R;
+
+public class CreateAccountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_account);
+    }
+}
