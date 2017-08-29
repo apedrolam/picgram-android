a1872d2c2a42260d76cba75dc128cafb9ca7e3cc
Albert Pedrola <abert.pedrola@netcentric.biz>
2017-08-29 22:37:17 +0200

    Redirect on tab the logo

--- app/src/main/java/com/apedrolam/picgram/LoginActivity.java
+++ app/src/main/java/com/apedrolam/picgram/LoginActivity.java
@@ -1,6 +1,7 @@
 package com.apedrolam.picgram;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -23,6 +24,11 @@ public class LoginActivity extends AppCompatActivity {
 
     public void goHome(View view) {
         Intent intent = new Intent(this, ContainerActivity.class);
+        startActivity(intent);
+    }
+
+    public void goWeb(View view) {
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://apedrolam.github.io"));
         startActivity(intent);
     }
 }
