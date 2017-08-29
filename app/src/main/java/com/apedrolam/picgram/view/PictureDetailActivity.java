a292d328f1f97fe74b05df86c0cab604ca680ef1
Albert Pedrola <abert.pedrola@netcentric.biz>
2017-08-29 21:06:53 +0200

    Adding transitions to picture detail

--- app/src/main/java/com/apedrolam/picgram/view/PictureDetailActivity.java
+++ app/src/main/java/com/apedrolam/picgram/view/PictureDetailActivity.java
@@ -1,9 +1,12 @@
 package com.apedrolam.picgram.view;
 
+import android.graphics.Color;
+import android.os.Build;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.transition.Fade;
 import android.view.View;
 
 import com.apedrolam.picgram.R;
@@ -14,8 +17,12 @@ public class PictureDetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_picture_detail);
-
         showToolbar("", true);
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            getWindow().setStatusBarColor(Color.TRANSPARENT);
+            getWindow().setEnterTransition(new Fade());
+        }
     }
 
     public void showToolbar(String title, boolean upButton) {
